a0955607c89a794d805556a0fe913af1a1817fe7
boyninja15 <flappyfloorg@gmail.com>
2024-11-10 09:43:54 -0800

    modified:   docs/source/main.js

--- docs/source/main.js
+++ docs/source/main.js
@@ -1,4 +1,4 @@
-import { createElement } from "@modules/Elements"
+import { createElement } from "@modules/Elements.js"
 
 export default () => {
     const title = createElement("h1", "Hello, World!", { id: "title" })
